9da23839fb46237d3afca6463d7aac6e9195e50c
Luke Hirst <521480@chrome.escg.ac.uk>
2023-11-13 13:11:00 +0000

    Lunchtime Commit
    Committing to save progress

--- Assets/Scripts/Audio/VolumeSettings.cs
+++ Assets/Scripts/Audio/VolumeSettings.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class VolumeSettings : MonoBehaviour
 {
     public AudioMixer master;
     public Slider musicSlider;
     public Slider sfxSlider;
+    public TextMeshProUGUI MusicVolText;
+    public TextMeshProUGUI SFXVolText;
+
+    public const string MIXER_MUSIC = "MusicVol";
+    public const string MIXER_SFX = "SFXVol";
 
     void Awake()
     {
-        musicSlider.onValueChanged.AddListener();
-        sfxSlider.onValueChanged.AddListener();
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY, musicSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, sfxSlider.value);
+    }
+
+    void SetMusicVolume(float value)
+    {
+        master.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+
+        MusicVolText.text = value.ToString("0.00");
+    }
+
+    void SetSFXVolume(float value)
+    {
+        master.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
+
+        SFXVolText.text = value.ToString("0.00");
     }
 }
---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public AudioMixer master;
+    public Sound[] sounds;
+
+    public const string MUSIC_KEY = "MusicVol";
+    public const string SFX_KEY = "SFXVol";
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        LoadVol();
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "Was not found! Are you typing it in correctly?");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "Was not found! Are you typing it in correctly?");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void LoadVol() // Volume from VolumeSettings.cs
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+
+        master.SetFloat(VolumeSettings.MIXER_MUSIC, Mathf.Log10(musicVolume) * 20);
+        master.SetFloat(VolumeSettings.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
+    }
+}
