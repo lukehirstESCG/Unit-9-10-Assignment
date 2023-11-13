66e7cfd9d5b0aef83354583d708c8f8584ed5f58
Luke Hirst <521480@chrome.escg.ac.uk>
2023-11-13 14:06:36 +0000

    FrontEnd more or less complete.

---
+++ Assets/Scripts/Utility/TitleScreen.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
